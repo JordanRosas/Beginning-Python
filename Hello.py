b628fde5edf8c516034f00142e2c185aa7b938b6
Jordan Rosas <jordanrosas1203@gmail.com>
2019-04-25 22:09:09 -0500

    uncommented code

--- Hello.py
+++ Hello.py
@@ -1,21 +1,19 @@
-# print("Hello World")
+print("Hello World")
 
-# name = "Jordan"
-# print("Hello! my name is " + name)
+name = "Jordan"
+print("Hello! my name is " + name)
 
-# age = int(input("Enter Your age "))
-# name = input("Enter Your name ")
-# print("Your name is " + name + " you are " + str(age) + " years old")
+age = int(input("Enter Your age "))
+name = input("Enter Your name ")
+print("Your name is " + name + " you are " + str(age) + " years old")
 
-#Fizz Buzz challenge in python! 
-# for i in range (101):
-#   if (i % 3 == 0 & i % 5 == 0):
-#     print("Fizz Buzz")
-#   elif(i % 3 == 0):
-#     print("Fizz")
-#   elif(i % 5 == 0):
-#     print("Buzz")
-#   else:
-#     print(i)
-
-#
+Fizz Buzz challenge in python! 
+for i in range (101):
+  if (i % 3 == 0 & i % 5 == 0):
+    print("Fizz Buzz")
+  elif(i % 3 == 0):
+    print("Fizz")
+  elif(i % 5 == 0):
+    print("Buzz")
+  else:
+    print(i)
